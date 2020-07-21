828e4dfcefea3044b60606c27bf5ccdabdbd7dc5
imseongtae <dev.haemil@gmail.com>
2020-07-22 00:14:58 +0900

    chore: Prepare project to include config setting

--- bin/www.js
+++ bin/www.js
@@ -1,9 +1,24 @@
+const http = require('http');
+
 const syncDb = require('./sync-db');
 const app = require('../app');
+const confg = require('../config/config.json');
 
+app.server = http.createServer(app);
+
+const hostname = confg.development.host;
 syncDb().then(() => {
-	console.log('database 작동 중');
-	app.listen(3000, () => {
-		console.log('pcs back-end 작동 중');
+	console.log(
+		'--------------------------------------------------------------------------------',
+	);
+	console.log('Database 작동 중');
+	app.server.listen(process.env.PORT || confg.port, hostname, () => {
+		console.log(
+			'--------------------------------------------------------------------------------',
+		);
+		console.log(`Server running at http://${hostname}:${confg.port}/`);
+		console.log(
+			'--------------------------------------------------------------------------------',
+		);
 	});
 });

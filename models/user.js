02825491a849791706026d375f6dec20ccdd2db4
imseongtae <dev.haemil@gmail.com>
2020-07-22 06:29:45 +0900

    chore: Set User API model

--- models/user.js
+++ models/user.js
@@ -1,19 +1,41 @@
+function isGoogleDomain(domain) {
+	const googleDomain = '@gmail.com';
+
+	if (googleDomain === domain) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
 module.exports = (sequelize, DataTypes) => {
 	const User = sequelize.define(
 		'User',
 		{
 			email: {
 				type: DataTypes.STRING(40),
-				allowNull: false, // 필수
-				unique: true, // 중복 금지
+				allowNull: false,
+				unique: true,
+				validata: {
+					isGmail(inputtedEmail) {
+						const inputtedEmailDomain = inputtedEmail.slice(-10).toLowerCase();
+						if (!isGoogleDomain(inputtedEmailDomain)) {
+							throw new Error('이메일 형식이 맞지 않습니다.');
+						}
+					},
+				},
 			},
 			password: {
 				type: DataTypes.STRING(100),
 				allowNull: false,
 			},
 			nickname: {
-				type: DataTypes.STRING(100),
+				type: DataTypes.STRING,
 				allowNull: false,
+			},
+			isAdmin: {
+				defaultValue: false,
+				type: DataTypes.BOOLEAN,
 			},
 		},
 		{

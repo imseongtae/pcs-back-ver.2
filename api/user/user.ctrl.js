1fd9793b1078759323e053e66a9b6def0d49121e
imseongtae <dev.haemil@gmail.com>
2020-07-23 06:52:28 +0900

    feat: Add api and test case
    - Add api to control reading
    - Add test case for create user api
    - Add test case for read user api

--- api/user/user.ctrl.js
+++ api/user/user.ctrl.js
@@ -6,7 +6,8 @@ const index = async (req, res) => {
 		// parseInt는 정수가 아니면 NaN을 반환
 		const limit = parseInt(req.query.limit, 10);
 		// return 을 해주어야 아래로 진행을 막을 수 있다
-		if (Number.isNaN(limit)) return res.status(400).end();
+		if (Number.isNaN(limit))
+			return res.status(400).json({ msg: 'limit 값이 숫자형이 아님' });
 
 		// const users = await User.findAll({ limit });
 		const users = await User.findAll({
@@ -22,30 +23,62 @@ const index = async (req, res) => {
 	}
 };
 
-const create = async (req, res) => {
-	const nickname = req.body.nickname;
-	if (!nickname) return res.status(400).end();
-
-	const email = req.body.email;
-	const password = req.body.password;
-
+const read = async (req, res) => {
 	try {
+		// if (parseInt(id, 10)) return res.status(400).end();
+		const id = parseInt(req.params.id, 10);
+		if (Number.isNaN(id))
+			return res.status(400).json({ msg: '매개변수가 숫자가 아님' });
+		const user = await User.findOne({ where: { id } });
+		if (!user) return res.status(404).json({ msg: 'do not find user' });
+
+		res.status(200).json({ data: user });
+	} catch (error) {
+		console.log(error);
+		res.status(500).end();
+	}
+};
+
+const create = async (req, res) => {
+	try {
+		const nickname = req.body.nickname;
+		if (!nickname) return res.status(400).json({ msg: 'no nickname' });
+		const email = req.body.email;
+		if (!email) return res.status(400).json({ msg: 'no email' });
+		const password = req.body.password;
+		if (!password) return res.status(400).json({ msg: 'no password' });
+		// 생성할 user data
 		const newUser = {
 			email,
 			password,
 			nickname,
 		};
-		console.log('데이터 생성을 위한 newUser: ', newUser);
+		// 중복일 경우 catch에서 처리됨
 		const user = await User.create(newUser);
-		console.log('생성된 데이터 user: ', user);
-		res.status(201).json(user);
+		// console.log('생성된 데이터 user: ', user);
+		res.status(201).json({ data: user });
 	} catch (error) {
+		// 모델에서 Unique로 설정된 컬럼이 있을 경우 반환하는 error
+		if (error.name === 'SequelizeUniqueConstraintError') {
+			return res.status(409).json({ msg: 'Duplicated email' });
+		}
 		console.log(error);
 		return res.status(500).end();
 	}
 };
 
+// const destroy = async (req, res) => {
+// 	try {
+// 		const id = parseInt(req.params.id, 10);
+// 		if (Number.isNaN(id))
+// 			return res.status(400).json({ msg: '매개변수가 숫자가 아님' });
+// 	} catch (error) {
+// 		console.log(error);
+// 	}
+// };
+
 module.exports = {
 	index,
+	read,
 	create,
 };
--- api/user/user.spec.js
+++ api/user/user.spec.js
@@ -14,32 +14,29 @@ const assert = require('assert')
 //   };
 // }
 
-// function hasData(status, callback) { // 1번
-//   return (res) => { // 2번
-//     assert.equal(res.status, status); // 3번 
-//     if (!body.hasOwnProperty('data')) throw new Error("missing data key"); // 4번    
-//     callback(res.data); // 5번 
-//   };
-// }
-
+/**
+ * 참조
+ * http://jeonghwan-kim.github.io/dev/2020/05/25/supertest.html
+ * https://shouldjs.github.io/
+ */
 describe('GET /users', () => {
   before(() => models.sequelize.sync({ force: true }));
-	const users = [
-		{
-			email: 'haemil@gmail.com',
-			password: '1234',
-			nickname: 'ham',
-		},
-		{
-			email: 'sonaldo@gmail.com',
-			password: '1234',
-			nickname: 'son',
-		},
-		{
-			email: 'messi@gmail.com',
-			password: '1234',
-			nickname: 'messi',
-		},
+  const users = [
+    {
+      email: 'haemil@gmail.com',
+      password: '1234',
+      nickname: 'ham',
+    },
+    {
+      email: 'sonaldo@gmail.com',
+      password: '1234',
+      nickname: 'son',
+    },
+    {
+      email: 'messi@gmail.com',
+      password: '1234',
+      nickname: 'messi',
+    },
   ];
   before(() => models.User.bulkCreate(users))
   describe('성공시', () => {
@@ -47,30 +44,137 @@ describe('GET /users', () => {
       request(app).get('/users').expect(200).end(done)
     })
     it('유저 객체를 담은 배열을 응답한다.', done => {
-			request(app)
-        .get('/users')                
-				.end((err, res) => {
+      request(app)
+        .get('/users')
+        .end((err, res) => {
           // key: value를 맞추기 위해 body.data로 테스트
-          res.body.data.should.be.instanceOf(Array)          
-					done();
+          res.body.data.should.be.instanceOf(Array)
+          done();
         });
-        // .expect(200, {data: users}).end(done)
+      // .expect(200, {data: users}).end(done)
     });
     it('설정한 limit 개수만큼 응답한다.', done => {
       request(app)
         .get('/users?limit=2')
-        .end((err, res) => {      
+        .end((err, res) => {
           // console.log('data가 무엇인지:', res) // 아래처럼 날라옴
           //  body: { data: [ [Object], [Object] ] },
           res.body.data.should.have.lengthOf(2);
           done();
-        })        
+        })
     })
   })
   describe('실패시', () => {
-		it('limit이 숫자형이 아니면 400을 응답한다.', done => {
-			request(app).get('/users?limit=two').expect(400).end(done);
-		});
-	});
+    it('limit이 숫자형이 아니면 400을 응답한다.', done => {
+      request(app).get('/users?limit=two').expect(400).end(done);
+    });
+  });
 })
 
+describe('GET /users/:id', () => {
+  describe('성공시', () => {
+    it('id가 1인 user 객체를 반환한다.', done => {
+      request(app).get('/users/1')
+        .end((err, res) => {
+          res.body.data.should.have.property('id', 1)
+          done();
+        })
+    })
+  });
+  describe('실패시', () => {
+    it('라우팅 매개변수 숫자가 아닐 경우 400으로 응답한다.', done => {
+      request(app)
+        .get('/users/one')
+        .expect(400).end(done);
+    })
+    it('id로 사용자를 찾을 수 없는 경우 404로 응답한다.', done => {
+      request(app)
+        .get('/users/999')
+        .expect(404)
+        .end(done)
+    })
+  });
+});
+
+describe('POST /users', () => {
+  before(() => models.sequelize.sync({ force: true }));
+  const users = [
+    {
+      email: 'haemil@gmail.com',
+      password: '1234',
+      nickname: 'ham',
+    },
+    {
+      email: 'sonaldo@gmail.com',
+      password: '1234',
+      nickname: 'son',
+    },
+    {
+      email: 'messi@gmail.com',
+      password: '1234',
+      nickname: 'messi',
+    },
+  ];
+  before(() => models.User.bulkCreate(users))
+
+  describe('성공시', () => {
+    let body;
+    const newUser = {
+      "email": "hamburger@naver.com",
+      "password": "1234",
+      "nickname": "hamburger"
+    }; 
+    before(done => {
+      request(app).post('/users')
+        .send(newUser)
+        .expect(201)
+        .end((err, res) => {
+          body = res.body;
+          // console.log('body값..: ', body);
+          done();
+        })
+    })    
+    it('생성된 사용자 객체를 반환한다.', done => {
+      // body값을 조회하면 생성한 데이터가 조회됨
+      // console.log('body값..: ', body)     
+      body.data.should.have.property('id')
+      done();
+    });
+    it('입력한 nickname을 반환한다.', done => {
+      // 아래는 should가 출력하는 메시지
+      // operator: 'to have property nickname of \'hamburger\'' } }
+      body.data.should.have.property('nickname', newUser.nickname)
+      done()
+    })
+  })
+  describe('실패시', () => {
+    it('파라미터 누락시 400을 반환한다.', done => {
+      const newUser = {
+        "email": "",
+        "password": "1234",
+        "nickname": "hamburger"
+      }; 
+      request(app).post('/users').send(newUser).expect(400).end(done)
+    });
+    it('email이 중복일 경우 409를 반환한다.', done => {
+      const newUser = {
+        "email": "hamburger@naver.com",
+        "password": "1234",
+        "nickname": "hamburger"
+      };
+      request(app)
+        .post('/users')
+        .send(newUser)
+        .expect(409)
+        .end(done)
+    })
+  })
+})
+
+// describe('DELETE /users/:id', () => {
+// })
+
+// describe('POST /users', () => {
+//   describe('성공시')
+//   describe('실패시')
+// })

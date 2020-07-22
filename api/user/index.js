423fd6b243c52e7cf63d5c1d5ad89d7ddf8c8991
imseongtae <dev.haemil@gmail.com>
2020-07-23 07:06:22 +0900

    feat: Add router for user reading

--- api/user/index.js
+++ api/user/index.js
@@ -3,8 +3,11 @@ const router = Router();
 
 const ctrl = require('./user.ctrl');
 
+// index
 router.get('/', ctrl.index);
-
+// read user
+router.get('/:id', ctrl.read);
+// create user
 router.post('/', ctrl.create);
 
 module.exports = router;

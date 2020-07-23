925e6924044ba8be181f2fc13f8ff4038e8df8c2
imseongtae <dev.haemil@gmail.com>
2020-07-24 07:51:54 +0900

    style: Update code format

--- api/facility/scripts/index.js
+++ api/facility/scripts/index.js
@@ -1,6 +1,31 @@
 const fs = require('fs');
 const { Facility, Facility_detail } = require('../../../models');
 
+// DB에 저장
+const storeFacilityData = async data => {
+	try {
+		await Promise.all(
+			data.map(async v => {
+				return await Facility_detail.upsert({
+					mt10id: v.mt10id._text,
+					fcltynm: v.fcltynm._text,
+					mt13cnt: v.mt13cnt._text,
+					fcltychartr: v.fcltychartr._text,
+					opende: v.opende._text,
+					seatscale: v.seatscale._text,
+					telno: v.telno._text,
+					relateurl: v.relateurl._text,
+					adres: v.adres._text,
+					la: v.la._text,
+					lo: v.lo._text,
+				});
+			}),
+		);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
 const getDummyData = async () => {
 	// test case 를 위한 dummy data
 	// fs 모듈 메서드 인자로 절대 경로 전달
@@ -26,30 +51,7 @@ const getDummyData = async () => {
 	}
 };
 
-// DB에 저장
-const storeFacilityData = async data => {
-	try {
-		await Promise.all(
-			data.map(async v => {
-				return await Facility_detail.upsert({
-					mt10id: v.mt10id._text,
-					fcltynm: v.fcltynm._text,
-					mt13cnt: v.mt13cnt._text,
-					fcltychartr: v.fcltychartr._text,
-					opende: v.opende._text,
-					seatscale: v.seatscale._text,
-					telno: v.telno._text,
-					relateurl: v.relateurl._text,
-					adres: v.adres._text,
-					la: v.la._text,
-					lo: v.lo._text,
-				});
-			}),
-		);
-	} catch (error) {
-		console.log(error);
-	}
-};
+// const getDummyDataForDetail = async () => {};
 
 module.exports = {
 	getDummyData,

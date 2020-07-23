4bd6d67c2fcc94d05d8b89fb3f896883bc22cb88
imseongtae <dev.haemil@gmail.com>
2020-07-23 19:26:40 +0900

    feat: Add router for memo index and read feature

---
+++ api/memo/index.js
@@ -0,0 +1,17 @@
+const { Router } = require('express');
+const router = Router();
+
+const ctrl = require('./memo.ctrl');
+
+// index memo
+router.get('/', ctrl.index);
+// read memo
+router.get('/:id', ctrl.read);
+// create memo
+router.post('/', ctrl.create);
+// update memo
+// router.put('/:id', ctrl.update);
+// delete memo
+// router.delete('/:id', ctrl.destroy);
+
+module.exports = router;

7a152ccf960ceede62e57615e0484d2a75f012e4
imseongtae <dev.haemil@gmail.com>
2020-07-27 02:11:00 +0900

    chore: Modify variable name to match meaning and fetch tidyData func

--- src/api/facility/scripts/index.js
+++ src/api/facility/scripts/index.js
@@ -1,6 +1,30 @@
 const fs = require('fs');
 const { Facility, Facility_detail } = require('../../../models');
 
+// KOPIS에 쿼리 스크립트를 통해 조회한 데이터를 정돈하기 위해 사용되는 함수
+const tidyData = data => {
+	try {
+		const [result] = data.map(v => {
+			return {
+				mt10id: v.mt10id._text,
+				fcltynm: v.fcltynm._text,
+				mt13cnt: v.mt13cnt._text,
+				fcltychartr: v.fcltychartr._text,
+				opende: v.opende._text,
+				seatscale: v.seatscale._text,
+				telno: v.telno._text,
+				relateurl: v.relateurl._text,
+				adres: v.adres._text,
+				la: v.la._text,
+				lo: v.lo._text,
+			};
+		});
+		return result;
+	} catch (error) {
+		console.log(error);
+	}
+};
+
 // DB에 저장
 const storeFacilityData = async data => {
 	try {
@@ -26,7 +50,7 @@ const storeFacilityData = async data => {
 	}
 };
 
-const getDummyData = async () => {
+const getFacilitiesDummyData = async () => {
 	// test case 를 위한 dummy data
 	// fs 모듈 메서드 인자로 절대 경로 전달
 	const dummy = fs.readFileSync('./src/api/facility/json/facility_dummy.json'); // 공연중목록을 쌓을 거임
@@ -54,6 +78,7 @@ const getDummyData = async () => {
 // const getDummyDataForDetail = async () => {};
 
 module.exports = {
-	getDummyData,
+	tidyData,
 	storeFacilityData,
+	getFacilitiesDummyData,
 };

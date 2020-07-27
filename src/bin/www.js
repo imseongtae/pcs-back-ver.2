795f234dac6148f187bf724a7ffbb38c79d72832
imseongtae <dev.haemil@gmail.com>
2020-07-27 16:12:35 +0900

    feat: Emphasize server start message

--- src/bin/www.js
+++ src/bin/www.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const chalk = require('chalk');
 
 const syncDb = require('./sync-db');
 const app = require('../app');
@@ -8,17 +9,14 @@ app.server = http.createServer(app);
 
 const hostname = confg.development.host;
 syncDb().then(() => {
-	console.log(
-		'--------------------------------------------------------------------------------',
-	);
-	console.log('Database 작동 중');
+	console.log('=========================================');
+	console.log(`${chalk.white.bgHex('00546B').bold(`MySQL Database running`)}`);
 	app.server.listen(process.env.PORT || confg.port, hostname, () => {
 		console.log(
-			'--------------------------------------------------------------------------------',
+			`${chalk.white
+				.bgHex('41b883')
+				.bold(`Server running at http://${hostname}:${confg.port}/`)}`,
 		);
-		console.log(`Server running at http://${hostname}:${confg.port}/`);
-		console.log(
-			'--------------------------------------------------------------------------------',
-		);
+		console.log('=========================================');
 	});
 });

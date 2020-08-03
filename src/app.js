c585c80d5a1f6af419cb4b3e1d92658307ba9d42
imseongtae <dev.haemil@gmail.com>
2020-08-03 18:43:11 +0900

    feat: Use cors and cors options

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 // libs
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 // api
 const api = require('./api');
@@ -12,6 +13,13 @@ const app = express();
 if (process.env.NOED_ENV !== 'test') {
 	app.use(morgan('dev'));
 }
+// 특정 URL만 허용하고 싶을 때
+const corsOptions = {
+	origin: 'http://localhost:8080',
+	credentials: true,
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 

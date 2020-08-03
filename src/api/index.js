628724a91412cdec8aebf53362ca1c3747135137
imseongtae <dev.haemil@gmail.com>
2020-08-03 18:44:25 +0900

    feat: Use passport init method for user login API

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,24 @@
 const { Router } = require('express');
 
+// passport
+const passportLib = require('../libs/passport');
+
+// API
 const UserAPI = require('./user');
+const AuthAPI = require('./auth');
 const MemoAPI = require('./memo');
 const FacilityAPI = require('./facility');
 const PerformanceAPI = require('./performance');
 
 module.exports = () => {
+	// passport init() 메서드를 통해 로그인 가능
+	passportLib.init();
+
 	const API = Router();
 
 	// routes setting
 	API.use('/users', UserAPI);
+	API.use('/auth', AuthAPI);
 	API.use('/memos', MemoAPI);
 	API.use('/facilities', FacilityAPI);
 	API.use('/performances', PerformanceAPI);

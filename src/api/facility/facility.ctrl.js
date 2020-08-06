44360653d050a7a43aa4397797ef6328650be6c7
imseongtae <dev.haemil@gmail.com>
2020-08-06 22:34:40 +0900

    feat: Modify facility controller
    - To active getFacilityDummyData function
    - To modify return value format

--- src/api/facility/facility.ctrl.js
+++ src/api/facility/facility.ctrl.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const { Facility, Facility_detail } = require('../../models');
 const { storeFacilityData, tidyData } = require('./scripts');
 // 처음 faciliy API에 데이터가 없다면 아래 함수 실행을 통해 더미 데이터를 등록한다.
-// const { getFacilitiesDummyData } = require('./scripts');
+const { getFacilitiesDummyData } = require('./scripts');
 
 // KOPIS에 쿼리스크립트를 요청하기 위해 필요한 URL
 const SERVICE_KEY = process.env.SERVICE_KEY;
@@ -20,12 +20,13 @@ const index = async (req, res) => {
 		if (Number.isNaN(limit))
 			return res.status(400).json('limit 값이 숫자형이 아님');
 
-		// await getFacilitiesDummyData(); // 초기 dummy data DB에 반영
+		await getFacilitiesDummyData(); // 초기 dummy data DB에 반영
 		const facilities = await Facility.findAll({ limit });
 		if (facilities.length === 0)
 			return res.status(404).json({ msg: 'not found facilities' });
 
-		return res.status(200).json({ data: facilities });
+		// return res.status(200).json({ data: facilities });
+		return res.status(200).json(facilities);
 	} catch (error) {
 		console.log(error);
 	}
@@ -39,7 +40,8 @@ const read = async (req, res) => {
 		// 목록 정보 대신 상세 정보 조회를 위해 쿼리 스크립트 작성
 		const facility = await Facility_detail.findByPk(mt10id);
 		if (facility) {
-			return res.status(200).json({ data: facility });
+			// return res.status(200).json({ data: facility });
+			return res.status(200).json(facility);
 		} else {
 			// 공연 시설 정보가 DB에 등록되어 있지 않을 때 KOPIS API에 직접 요청
 			const requestUrl = `${HOST}${category}${mt10id}?service=${SERVICE_KEY}`;
@@ -58,7 +60,8 @@ const read = async (req, res) => {
 					storeFacilityData([db]);
 					// 데이터 정돈
 					const result = tidyData([db]);
-					return res.status(200).json({ data: result });
+					// return res.status(200).json({ data: result });
+					return res.status(200).json(result);
 				} else {
 					return res.status(404).json({ msg: 'not found facility' });
 				}
